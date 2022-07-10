d5dc3b5e5e701014c4074e4ff59b82b62a6ab878
hanna <hanna@pyxel.one>
2022-07-10 18:48:41 -0400

    feat: basic cli with help message and usage

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
+const args = @import("args");
+const help = @import("help.zig");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const stdout = std.io.getStdOut();
+    const allocator = std.heap.c_allocator;
+
+    const cli = try args.parseForCurrentProcess(struct {
+        pub const shorthands = .{ .h = "help", .v = "version" };
+        version: bool = false,
+        help: bool = false,
+    }, allocator, .silent);
+    defer cli.deinit();
+
+    if (cli.options.help) try help.print(stdout, .full);
+    if (cli.options.version) try help.print(stdout, .version);
+    if (cli.positionals.len <= 0) try help.print(stdout, .usage);
 }
