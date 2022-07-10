408f8c6a72b429e595a8e9c2f4526c2a32d15199
hanna <hanna@pyxel.one>
2022-07-10 18:28:41 -0400

    build: pass current git ref to options package

--- build.zig
+++ build.zig
@@ -8,10 +8,16 @@ const _target = std.zig.CrossTarget{
     .os_tag = b_target.os.tag,
 };
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) anyerror!void {
     const exe = b.addExecutable("toxin", "src/main.zig");
     const target = b.standardTargetOptions(.{ .default_target = _target });
     const mode = b.standardReleaseOptions();
+
+    const options = b.addOptions();
+    const ref = try b.exec(&.{ "git", "rev-parse", "--short", "HEAD" });
+    options.addOption([]const u8, "revision", ref);
+    exe.addOptions("options", options);
+
     exe.setBuildMode(mode);
     exe.setTarget(target);
     exe.install();
