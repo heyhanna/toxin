63e62e5a7cd8b6189a1932a91725b6218174c75c
hanna <hanna@pyxel.one>
2022-07-10 18:06:34 -0400

    build: change default built target to use musl

--- build.zig
+++ build.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
+const b_target = @import("builtin").target;
+
+const _target = std.zig.CrossTarget{
+    .abi = if (b_target.os.tag == .linux) .musl else .gnu,
+    .cpu_model = .{ .explicit = b_target.cpu.model },
+    .cpu_arch = b_target.cpu.arch,
+    .os_tag = b_target.os.tag,
+};
 
 pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("toxin", "src/main.zig");
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{ .default_target = _target });
     const mode = b.standardReleaseOptions();
     exe.setBuildMode(mode);
     exe.setTarget(target);
